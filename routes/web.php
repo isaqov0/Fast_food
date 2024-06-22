33504f71eb6fb1e63ac69a86657a5647d98792ba
isakov <isakov0017@gmail.com>
2024-06-22 16:16:16 +0500

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
+	
+	
+	use App\Http\Controllers\FastFoodController;
+	use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FastFoodController::class , 'index']);
